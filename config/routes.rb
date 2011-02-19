de101aaf2246baebf5dc34f23e19929f0a6b80a5
Paweł Dawczak <pawel.dawczak@gmail.com>
2011-02-19 19:45:19 +0100

    Finished layout and route

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 SampleApp::Application.routes.draw do
-  # get "pages/home"
-  # get "pages/contact"
-  # get "pages/about"
-  # get "pages/help"
+  get "users/new"
+  
+  match '/signup',  :to => 'users#new'
   
   match '/contact', :to => 'pages#contact'
   match '/about',   :to => 'pages#about'
